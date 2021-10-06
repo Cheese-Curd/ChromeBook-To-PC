c6d5f65fecc36670e61fd9e825309ca733e4bd08
Cheese Curd <51841108+Cheese-Curd@users.noreply.github.com>
2021-10-06 12:51:47 -0400

    Some FNF links

---
+++ FNF-Links.js
@@ -0,0 +1,15 @@
+var my_site = 'cheese-curd.github.io/secret/games/fnf/newgrounds-port-angel'
+/*
+	unlikely to get closed (when I change the name lol idea: make it canvas lmfao) 
+	change 'newgrounds-port-angel' to another mod in that directory IE: 'newgrounds'
+	note: straight newgrounds copy is not finished atm only have the fuckin' Funkin.js  and index.html
+	(assets coming soon™)
+*/
+ var KBHGames = 'kbhgames.com/game/friday-night-funkin' // Will probably get blocked or closed.
+var Newgrounds =  'http://uploads.ungrounded.net/alternate/1528000/1528775_alternate_113347_r86.zip/'
+/* 
+	Probably won't get closed, unless they see the tab itself also may or may not work since newgrounds is blocked.
+	Ungrounded is NOT blocked, at least the root directory.
+	Okay so different blocker, but it is blocked so sh-t got to use KBHGames
+	so, the blocked seems to be a blocker on the router, not a thing admin disabled for everyone (like a 18+ site) so there is hope that I can play FNF on the newgrounds site but honestly not likly, if twitter has this same block then I'll update and say that it is a router block, and not a extention block. SAME BLOCK! SO YES IT IS JUST BLOCKED ON THE ROUTER WOOOOoo, also funny thing it's called an "Application" cause Twitter is a web application like (big ass comment lmfao)
+*/
